e32ad152ac247c05a858f3f6cefae1a8bb4a77fa
AliRahimian110 <49687027+AliRahimian110@users.noreply.github.com>
2019-04-22 17:54:41 +0430

    AdminPage

--- Tafsir/Admin/AdminPage.Master.cs
+++ Tafsir/Admin/AdminPage.Master.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.IO;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace TafsirAdmin
+namespace TafsirAdmin 
 {
     public partial class Home : System.Web.UI.MasterPage
     {
