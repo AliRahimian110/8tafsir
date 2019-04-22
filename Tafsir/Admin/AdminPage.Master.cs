70019b77588eaaecef8269b7cca83b80cf719b85
AliRahimian110 <49687027+AliRahimian110@users.noreply.github.com>
2019-04-22 16:04:58 +0430

    Rubuild

---
+++ Tafsir/Admin/AdminPage.Master.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TafsirAdmin
+{
+    public partial class Home : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.Url.LocalPath.ToLower() == "/index.aspx")
+            {
+                container.Visible = false;
+            }
+
+            var user = (string)Session["UserAuthentication"] ?? "";
+
+            if (user.Length > 0)
+            {
+                menoLogout.Visible = false;
+                menoLogin.Visible = true;
+                //txtusername.InnerText = user;
+            }
+            else
+            {
+                menoLogout.Visible = true;
+                menoLogin.Visible = false;
+                //txtusername.InnerText = " ";
+            }
+        }
+    }
+}
