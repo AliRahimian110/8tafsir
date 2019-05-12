cabff757740eb686d79e651185ea334909b12cb4
AliRahimian110 <49687027+AliRahimian110@users.noreply.github.com>
2019-05-12 23:05:10 +0430

    add TeacherList

---
+++ Tafsir/TeacherList.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Tafsir
+{
+    public partial class TeacherList : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                ListView5.DataSource = new TafsirLib.Teacher().Load();
+                ListView5.DataBind();
+            }
+            catch (Exception)
+            {
+                //var err = ex.Message;
+            }
+        }
+    }
+}
