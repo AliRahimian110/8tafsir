0f4199f9efd48157a99548910f70744c3d4eeca5
alirahimian110 <rahimian110@gmil.com>
2019-05-22 23:35:22 +0430

    Add commentslist

---
+++ Tafsir/Admin/CommentsList.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tafsir.Admin
+{
+    public partial class CommentsList : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                ListView1.DataSource = new TafsirLib.Comments().Load();
+                ListView1.DataBind();
+            }
+            catch (Exception)
+            {
+                //var err = ex.Message;
+            }
+        }
+    }
+}
