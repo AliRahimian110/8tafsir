9442ba1910d958691bb8474253acc51f041f2524
alirahimian110 <rahimian110@gmil.com>
2019-05-22 23:09:46 +0430

    Add page QuestionList - StudentList - UserList

---
+++ Tafsir/Admin/QuestionNazarsanjiList.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tafsir.Admin
+{
+    public partial class QuestionNazarsanjiList : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                ListView1.DataSource = new TafsirLib.QuestionNazarsanji().Load();
+                ListView1.DataBind();
+            }
+            catch (Exception)
+            {
+                //var err = ex.Message;
+            }
+        }
+    }
+}
