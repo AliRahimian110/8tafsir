ee0248a4ad6fdfe3de64392681126646332c129f
alirahimian110 <rahimian110@gmil.com>
2019-07-08 17:29:16 +0430

    CommentInfo

---
+++ Tafsir/Admin/CommentInfo.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Tafsir.Admin
+{
+    public partial class CommentInfo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void butUpData_OnClick(object sender, EventArgs e)
+        {
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
